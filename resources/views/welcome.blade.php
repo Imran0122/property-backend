05ca28e36347fd3da6a2bbfb3b05034a333c2d63
Imran <itecskill0@gmail.com>
2026-01-20 16:37:29 +0500

    Removed Vite directive to fix 500 server error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
     <title>Property Website</title>
-    @vite('resources/js/app.js')
+    
 </head>
 <body class="bg-gray-100 flex items-center justify-center h-screen">
 
