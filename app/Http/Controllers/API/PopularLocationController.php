bc2c98557e377e0ac31bf6c96e2cceeb29de8753
Imran <itecskill0@gmail.com>
2026-02-12 15:48:39 +0500

    Updated project files

--- app/Http/Controllers/API/PopularLocationController.php
+++ app/Http/Controllers/API/PopularLocationController.php
@@ -4,42 +4,45 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Property;
+use App\Models\PropertyType;
 use Illuminate\Support\Facades\DB;
 
 class PopularLocationController extends Controller
 {
     public function index()
     {
-        return response()->json([
-            'sale' => [
-                'homes'  => $this->getData('sale', 'Homes'),
-                'plots'  => $this->getData('sale', 'Plots'),
-                'commercial' => $this->getData('sale', 'Commercial'),
-            ],
-            'rent' => [
-                'homes'  => $this->getData('rent', 'Homes'),
-                'plots'  => $this->getData('rent', 'Plots'),
-                'commercial' => $this->getData('rent', 'Commercial'),
-            ],
-        ]);
-    }
+        $purposes = ['sale', 'rent'];
+        $propertyTypes = PropertyType::pluck('name');
 
-    private function getData($purpose, $propertyTypeName)
-    {
-        return Property::select(
-                'cities.name as city',
-                'areas.name as location',
-                DB::raw('COUNT(properties.id) as total')
-            )
-            ->join('cities', 'cities.id', '=', 'properties.city_id')
-            ->join('areas', 'areas.id', '=', 'properties.area_id')
-            ->join('property_types', 'property_types.id', '=', 'properties.property_type_id')
-            ->where('properties.status', 'active')
-            ->where('properties.purpose', $purpose)
-            ->where('property_types.name', $propertyTypeName)
-            ->groupBy('cities.name', 'areas.name')
-            ->orderByDesc('total')
-            ->limit(8)
-            ->get();
+        $data = [];
+
+        foreach ($purposes as $purpose) {
+
+            foreach ($propertyTypes as $type) {
+
+                $results = Property::select(
+                        'cities.name as city',
+                        'areas.name as location',
+                        DB::raw('COUNT(properties.id) as total')
+                    )
+                    ->join('cities', 'cities.id', '=', 'properties.city_id')
+                    ->join('areas', 'areas.id', '=', 'properties.area_id')
+                    ->join('property_types', 'property_types.id', '=', 'properties.property_type_id')
+                    ->where('properties.status', 'active')
+                    ->where('properties.purpose', $purpose)
+                    ->where('property_types.name', $type)
+                    ->groupBy('cities.name', 'areas.name')
+                    ->orderByDesc('total')
+                    ->limit(8)
+                    ->get()
+                    ->groupBy('city');
+
+                if ($results->isNotEmpty()) {
+                    $data[$purpose][$type] = $results;
+                }
+            }
+        }
+
+        return response()->json($data);
     }
 }
